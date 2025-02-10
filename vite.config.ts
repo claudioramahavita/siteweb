dcb2db89e71334022951d61d04675fcbecd89627
creitic <claudioramahavita@gmail.com>
2025-02-10 22:47:20 +0300

    t

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/siteweb/",
+  //base: "/siteweb/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
